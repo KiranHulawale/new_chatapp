0a035e34d6a88b1f16bcc5e4152357564aa03401
KiranHulawale <97278477+KiranHulawale@users.noreply.github.com>
2022-08-05 13:49:15 +0530

    update settings.py
    updated db credentials

--- fundoo/fundoo/settings.py
+++ fundoo/fundoo/settings.py
@@ -103,10 +103,10 @@ CHANNEL_LAYERS = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'kiran',
-        'USER': 'admin',
-        'PASSWORD': '12345678',
-        'HOST': 'database-1.cb8btkvqpi1e.ap-south-1.rds.amazonaws.com',
+        'NAME': 'my_DB',
+        'USER': 'kiran',
+        'PASSWORD': '12345',
+        'HOST': 'database',
         'PORT': '3306',
     }
 }
